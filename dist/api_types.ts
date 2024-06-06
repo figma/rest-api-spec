e38f552cf8acb291fa0a2c9c313acce0973216e3
Alex Hurtado <113389645+ahurtado-figma@users.noreply.github.com>
2024-06-05 20:10:15 -0400

    Update rest-api-spec to 0.15.0 (#15)

--- dist/api_types.ts
+++ dist/api_types.ts
@@ -609,6 +609,11 @@ export type TypePropertiesTrait = {
   characterStyleOverrides: number[]
 
   /**
+   * Internal property, preserved for backward compatibility. Avoid using this value.
+   */
+  layoutVersion?: number
+
+  /**
    * Map from ID to TypeStyle for looking up style overrides.
    */
   styleOverrideTable: { [key: string]: TypeStyle }
@@ -1724,15 +1729,40 @@ export type Style = {
 }
 
 /**
- * This type is a string enum with the following possible values
+ * This type is a string enum with the following possible values:
  *
  * - `EASE_IN`: Ease in with an animation curve similar to CSS ease-in.
  * - `EASE_OUT`: Ease out with an animation curve similar to CSS ease-out.
  * - `EASE_IN_AND_OUT`: Ease in and then out with an animation curve similar to CSS ease-in-out.
  * - `LINEAR`: No easing, similar to CSS linear.
- * - `GENTLE_SPRING`: Gentle spring animation similar to react-spring.
+ * - `EASE_IN_BACK`: Ease in with an animation curve that moves past the initial keyframe's value and
+ *   then accelerates as it reaches the end.
+ * - `EASE_OUT_BACK`: Ease out with an animation curve that starts fast, then slows and goes past the
+ *   ending keyframe's value.
+ * - `EASE_IN_AND_OUT_BACK`: Ease in and then out with an animation curve that overshoots the initial
+ *   keyframe's value, then accelerates quickly before it slows and overshoots the ending keyframes
+ *   value.
+ * - `CUSTOM_CUBIC_BEZIER`: User-defined cubic bezier curve.
+ * - `GENTLE`: Gentle animation similar to react-spring.
+ * - `QUICK`: Quick spring animation, great for toasts and notifications.
+ * - `BOUNCY`: Bouncy spring, for delightful animations like a heart bounce.
+ * - `SLOW`: Slow spring, useful as a steady, natural way to scale up fullscreen content.
+ * - `CUSTOM_SPRING`: User-defined spring animation.
  */
-export type EasingType = 'EASE_IN' | 'EASE_OUT' | 'EASE_IN_AND_OUT' | 'LINEAR' | 'GENTLE_SPRING'
+export type EasingType =
+  | 'EASE_IN'
+  | 'EASE_OUT'
+  | 'EASE_IN_AND_OUT'
+  | 'LINEAR'
+  | 'EASE_IN_BACK'
+  | 'EASE_OUT_BACK'
+  | 'EASE_IN_AND_OUT_BACK'
+  | 'CUSTOM_CUBIC_BEZIER'
+  | 'GENTLE'
+  | 'QUICK'
+  | 'BOUNCY'
+  | 'SLOW'
+  | 'CUSTOM_SPRING'
 
 /**
  * Individual stroke weights
